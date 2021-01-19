b37a280523adb91ab4b6ab94f8fb5a47d85128b8
mamtagarg2019 <mamta.rani01@trigma.in>
2021-01-19 11:46:46 +0530

    Login page UI

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+// import { Router, ActivatedRoute } from '@angular/router';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { first } from 'rxjs/operators';
+//import { AlertService, AuthenticationService } from '../_services';
+
+@Component({ templateUrl: 'login.component.html' })
+export class LoginComponent implements OnInit {
+   ngOnInit() {
+       
+   }
+}
